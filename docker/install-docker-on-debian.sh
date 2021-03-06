ff41fa9dfeca9e017fd9d8c776fb3e673fb82e02
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-03-06 14:13:13 +0800

    update docker and v2ray

--- docker/install-docker-on-debian.sh
+++ docker/install-docker-on-debian.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-set -e
+set -ex
 
-apt-get remove docker docker-engine docker.io containerd runc
+apt-get remove docker docker-engine docker.io containerd runc || true
 
 apt-get update
 
-apt-get install apt-transport-https ca-certificates curl gnupg
+apt-get install -y apt-transport-https ca-certificates curl gnupg
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 docker run hello-world
+
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
--- v2ray/README.md
+++ v2ray/README.md
@@ -5,10 +5,25 @@
 
 ### Install Docker Engine (Debian)
 
-[Official Document](https://docs.docker.com/engine/install/)
+The following script is from docker's [Official Document](https://docs.docker.com/engine/install/)
 
-## 生成UID
+```bash
+curl -fsSL https://raw.githubusercontent.com/zhangyuan/helpme/master/docker/install-docker-on-debian.sh | bash
+
+```
+## 生成配置文件
+
+生成UID
 
 ```bash
 python -c 'import uuid; print(str(uuid.uuid1()))'
+```
+
+Let's say UID is `6a11cd68-7e42-11eb-9ef3-acde48001122`,
+
+```bash
+# uuid is generate
+cp config/v2ray.example.json config/v2ray.json && sed -i '' 's/00000000000000000000/6a11cd68-7e42-11eb-9ef3-acde48001122/g' config/v2ray.example.json
+
+cp config/mkcp.example.json config/mkcp.json && sed -i '' 's/00000000000000000000/6a11cd68-7e42-11eb-9ef3-acde48001122/g' config/mkcp.example.json
 ```
